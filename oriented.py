31958439d1fbf7d006b3b579b4121b95da567e44
bbaruchel <barnabe.baruchel@gmail.com>
2026-06-30 10:20:33 +0200

    save

--- oriented.py
+++ oriented.py
@@ -14,7 +14,6 @@
 # ---
 
 # %%
-
 import pickle
 import pathlib
 import pandas as pd
@@ -84,12 +83,8 @@ seine3 = seine2[seine2["TopoOH"].isin(topo)]
 seine3
 
 # %%
-G = nx.Graph()
-
-for _, row in tqdm(seine3.iterrows(), total=len(seine3)): 
-    geom = row.geometry
-    for i, j in zip(geom.coords, geom.coords[1:]):
-        G.add_edge(i, j, weight=LineString([i, j]).length)
+G = pickle.load(open('graph.pickle', 'rb'))
+Gc = pickle.load(open('contract.pickle', 'rb'))
 
 # %%
 root_node = sites2.loc[sites2["Libellé"] == Root_Name, "geometry"]
@@ -103,20 +98,6 @@ for site in sites2.itertuples():
     attrs["label"] = site.Libellé
     site_nodes.append(coord)
 
-# %%
-colors = []
-paths = {}
-for site in site_nodes:
-    attrs = G.nodes[site]
-    if nx.has_path(G, root_node, site):
-        path = nx.shortest_path(G, root_node, site, weight="weight")
-        paths[site] = path
-        colors.append("green")
-    else:
-        print(f"Aucun chemin trouvé vers {attrs['label']}")
-        colors.append("red")
-
-# %%
 '''
 fig, ax = plt.subplots(figsize=(12, 12))
 seine3.plot(ax=ax, lw=0.5, color="tab:blue", zorder=-1)
@@ -138,53 +119,6 @@ m = gdf[["CdNoeudDeb", "CdNoeudFin"]].notna().all(axis=1)
 H = nx.from_pandas_edgelist(gdf[m].assign(length=gdf[m].length), source="CdNoeudDeb", target="CdNoeudFin", edge_attr=["length"])
 
 
-def contract(g):
-    """
-    Contract chains of neighbouring vertices with degree 2 into a single edge.
-
-    Arguments:
-    ----------
-    g -- networkx.Graph or networkx.DiGraph instance
-
-    Returns:
-    --------
-    h -- networkx.Graph or networkx.DiGraph instance
-        the contracted graph
-    """
-
-    # Travailler sur la version non orientée pour identifier les chaînes
-    ug = g.to_undirected() if g.is_directed() else g
-
-    # Créer le sous-graphe des nœuds de degré 2
-    is_chain = [node for node, degree in ug.degree() if ((degree == 2) and (G.nodes[node].get("site_id") is None))]
-    chains = ug.subgraph(is_chain)
-
-    # Correction : connected_component_subgraphs supprimé en NX 2.4+
-    # On applique connected_components sur chains (pas sur g)
-    components = [chains.subgraph(c) for c in nx.connected_components(chains)]
-
-    hyper_edges = []
-    for component in components:
-        end_points = [node for node, degree in component.degree() if degree < 2]
-        candidates = set(neighbor for node in end_points for neighbor in ug.neighbors(node))
-        connectors = list(candidates - set(component.nodes()))
-        weights = [data.get('weight', 1) for _, _, data in component.edges(data=True)]
-        hyper_edges.append((connectors, np.sum(weights)))
-
-    # Initialiser le nouveau graphe sans les nœuds de degré 2
-    not_chain = [node for node in g.nodes() if node not in is_chain]
-    h = g.subgraph(not_chain).copy()
-    for connectors, weight in hyper_edges:
-        # Garde-fou : une chaîne en bout de graphe peut avoir < 2 connecteurs
-        if len(connectors) == 2:
-            h.add_edge(connectors[0], connectors[1], weight=weight)
-
-    return h
-
-
-# Contraction
-G_contract = contract(G)
-
 '''
 
 # Plot
@@ -197,6 +131,4 @@ nx.draw(G_contract, pos={n: pos[n] for n in G_contract.nodes() if n in pos}, ax=
 plt.show()
 '''
 
-print(len(G_contract.nodes()), len(G_contract.edges()),len(G.nodes()), len(G.edges()))
-pickle.dump(G, open('graph.pickle', 'wb'))
-pickle.dump(Gc, open('contract.pickle', 'wb'))
+print(len(Gc.nodes()), len(Gc.edges()),len(G.nodes()), len(G.edges()))
